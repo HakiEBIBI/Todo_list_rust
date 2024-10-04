559a115171e0c39b488262a0a2cd4de69229544e
HakiEBIBI <haki.ebibi@jobtrek.ch>
2024-10-04 11:11:05 +0200

    feat: 4.2 ask to user to create and save their to-do list in todo-list.txt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{self};
+use std::fs::File;
+use std::io::prelude;
+use std::io::Write;
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::create("todo-list.txt")
+        .expect("could not create file!");
+    let mut input = String::new();
+    println!("Create your to do list");
+    io::stdin().read_line(&mut input).expect("error: unable to read user input");
+    file.write(input.as_bytes()).expect("we cannot create a file for your todo list");
+
+    println!("i created a file with the name of todo-list");
 }
