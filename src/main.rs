b50eaf14ddbe30d2f0bfae1cc02c4c8fd7bca9fb
HakiEBIBI <haki.ebibi@jobtrek.ch>
2024-10-08 13:00:16 +0200

    feat: 4.3 append a new todo line to the text file after after the user input

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use std::io::{self};
-use std::fs::File;
-use std::io::prelude;
-use std::io::Write;
+use std::fs::OpenOptions;
+use std::io::{self, Write};
 fn main() {
-    let mut file = File::create("todo-list.txt")
-        .expect("could not create file!");
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("todo-list.txt")
+        .expect("unable to open the file");
+
     let mut input = String::new();
-    println!("Create your to do list");
-    io::stdin().read_line(&mut input).expect("error: unable to read user input");
-    file.write(input.as_bytes()).expect("we cannot create a file for your todo list");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("error: unable to read user input");
+    let input = input.trim();
 
     println!("i created a file with the name of todo-list");
+    writeln!(file, "{}", input).expect("err");
 }
